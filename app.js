e64d62fb67827b4120d880dde9e68bf437947a41
Kaitlin <kaitlin.ramirez@colorado.edu>
2018-07-06 10:52:24 -0600

    delete request

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 const sheets = require("./routes/sheets");
 
 app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "GET, POST, OPTIONS, PUT, DELETE");
+   res.header("Access-Control-Allow-Origin", "DELETE");
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    next();
  });
